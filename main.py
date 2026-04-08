b379ede1f68d065e4680a3c70914d0a4d2840e47
25centeno <victoria76969@gmail.com>
2026-04-08 10:14:49 -0400

    Started the pyfun with hyper test marker language

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#this is the main python file
+#this is the main python file - owem
+
+# Imports #
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, text
+
+app = Flask(__name__)
+
+conn_str = "mysql://root:cset155@localhost"
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
