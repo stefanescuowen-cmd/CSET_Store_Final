5ad476a0a28b276ded42813f0e9915add2f5f776
calebzroberts <caleb.roberts798@gmail.com>
2026-04-09 10:56:55 -0400

    Fixed seed_data file

--- database/store_database_schema.sql
+++ database/store_database_schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE products (
     product_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
-    warranty_period INT NOT NULL, -- in months
+    warranty_period INT NOT NULL DEFAULT 12, -- in months
     price DECIMAL(10,2) NOT NULL,
     discount_price DECIMAL(10,2),
     discount_deadline DATETIME,
