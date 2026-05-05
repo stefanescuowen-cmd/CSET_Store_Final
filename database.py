7ff9e80459dac811add5944947259eafa558652c
Owen <stefanescuowen@gmail.com>
2026-05-05 11:09:50 -0400

    Enhance review sorting options by adding 'Oldest First' and 'Newest First' criteria

--- database.py
+++ database.py
@@ -950,7 +950,9 @@ def get_all_reviews(connection, product_id=None, sort_by='date', filter_rating=N
         params["rating"] = filter_rating
 
     # Sorting Logic
-    if sort_by == 'date':
+    if sort_by == 'date_oldest':
+        sql += " ORDER BY date ASC"
+    if sort_by == 'date_newest':
         sql += " ORDER BY r.date DESC"
     elif sort_by == 'rating_high':
         sql += " ORDER BY r.rating DESC"
